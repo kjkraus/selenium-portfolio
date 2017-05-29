ed53fab81158484479de7ccca25f7fe96c74559d
Kent Kraus <Kent.Kraus@greenwayhealth.com>
2017-05-29 19:30:45 -0400

    add GoTo method

---
+++ MedlinePlusAutomation/MedlinePlusAutomation/HomePage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedlinePlusAutomation
+{
+    public class HomePage
+    {
+        public static void GoTo()
+        {
+            var driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("https://medlineplus.gov/");
+        }
+
+        public static object SearchWith(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MedlinePlusAutomation/MedlinePlusTests/SearchTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MedlinePlusAutomation;
+
+namespace MedlinePlusTests
+{
+    [TestClass]
+    public class SearchTests
+    {
+        [TestMethod]
+        public void UserCanSearch()
+        {
+            HomePage.GoTo();
+            //HomePage.SearchWith("aspirin").Search();
+            //Part 1: First Test Creating...
+            //Assert.IsTrue(HomePage.IsAt, "Failed to reach home page.");
+        }
+    }
+}
