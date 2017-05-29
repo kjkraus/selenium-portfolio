c5d0a83afc927f81c9167dd6cde18bd68023aa93
Kent Kraus <Kent.Kraus@greenwayhealth.com>
2017-05-29 19:01:46 -0400

    add initial setup and firefox test

---
+++ MedlinePlusAutomation/MedlinePlusAutomation/Class1.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium.Firefox;
+
+namespace MedlinePlusAutomation
+{
+    public class Class1
+    {
+        public void Go()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://google.com");
+        }
+    }
+}
---
+++ MedlinePlusAutomation/MedlinePlusTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MedlinePlusAutomation;
+
+namespace MedlinePlusTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var c = new Class1();
+            c.Go();
+        }
+    }
+}
